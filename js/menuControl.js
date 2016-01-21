fd02f1668be7a374788f79f34806243d1734bbcc
Lucas Terra Vieira <lucasterravieira@gmail.com>
2016-01-20 20:01:46 -0800

    menu control working

--- js/menuControl.js
+++ js/menuControl.js
@@ -1,16 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    
     $("#lunch").hide();
     $("#dinner").hide();
     $("#aboutus").hide();
     $("#contactus").hide(); 
     $("#home").show();
+    
+    
+    $("#mlunch").click(function(){
+        $("#lunch").show();
+        $("#dinner").hide();
+        $("#aboutus").hide();
+        $("#contactus").hide(); 
+        $("#home").hide();
+    });
+    
+        
+    $("#mdinner").click(function(){
+        $("#lunch").hide();
+        $("#dinner").show();
+        $("#aboutus").hide();
+        $("#contactus").hide(); 
+        $("#home").hide();
+    });
+    
+     $("#maboutus").click(function(){
+        $("#lunch").hide();
+        $("#dinner").hide();
+        $("#aboutus").show();
+        $("#contactus").hide(); 
+        $("#home").hide();
+    });   
+    
+     $("#mcontactus").click(function(){
+        $("#lunch").hide();
+        $("#dinner").hide();
+        $("#aboutus").hide();
+        $("#contactus").show(); 
+        $("#home").hide();
+    });   
+    
+    
+     $("#mhome").click(function(){
+        $("#lunch").hide();
+        $("#dinner").hide();
+        $("#aboutus").hide();
+        $("#contactus").hide(); 
+        $("#home").show();
+    });   
 });
 
 
-$("#mcontactus").click(function() {
-    $("#lunch").hide();
-    $("#dinner").show();
-    $("#aboutus").hide();
-    $("#contactus").hide(); 
-    $("#home").hide();
-});
+
